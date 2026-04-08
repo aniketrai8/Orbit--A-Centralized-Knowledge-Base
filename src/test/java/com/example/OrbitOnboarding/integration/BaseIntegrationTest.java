f0d9c43db9010b4e3bff3fefe19864e95df920db
aniketrai8 <rai.aniket@outlook.com>
2026-04-08 23:20:50 +0530

    Updating testcontainers yml file

--- src/test/java/com/example/OrbitOnboarding/integration/BaseIntegrationTest.java
+++ src/test/java/com/example/OrbitOnboarding/integration/BaseIntegrationTest.java
@@ -9,7 +9,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@ActiveProfiles("test")
+@ActiveProfiles("testcontainers")
 @Import(org.testcontainers.utility.TestcontainersConfiguration.class)
 public abstract class BaseIntegrationTest extends ContainerPropertyInitializer{
 
