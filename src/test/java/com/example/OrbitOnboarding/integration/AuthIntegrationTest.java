fd429abda96cfa81442dc42febae9dd4add4944d
aniketrai8 <rai.aniket@outlook.com>
2026-04-08 19:33:24 +0530

    Added More IntegrationTests

--- src/test/java/com/example/OrbitOnboarding/integration/AuthIntegrationTest.java
+++ src/test/java/com/example/OrbitOnboarding/integration/AuthIntegrationTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
