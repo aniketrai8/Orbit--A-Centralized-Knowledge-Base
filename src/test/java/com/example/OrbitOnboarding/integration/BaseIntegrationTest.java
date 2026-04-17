36a49487e7c70db98a1b61eade8c16893e3011b6
aniketrai8 <rai.aniket@outlook.com>
2026-04-17 22:44:32 +0530

    Fixing Failing IT tests

--- src/test/java/com/example/OrbitOnboarding/integration/BaseIntegrationTest.java
+++ src/test/java/com/example/OrbitOnboarding/integration/BaseIntegrationTest.java
@@ -5,8 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Aims to provide a common annotation for all IT tests
@@ -14,6 +16,8 @@ import org.springframework.test.web.servlet.MockMvc;
 @Tag("integration")
 @SpringBootTest
 @AutoConfigureMockMvc
+@Transactional
+@Rollback
 @ActiveProfiles("testcontainers")
 @Import(org.testcontainers.utility.TestcontainersConfiguration.class)
 public abstract class BaseIntegrationTest extends ContainerPropertyInitializer{
